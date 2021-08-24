4ea1f2750175dd68b1210384dead8bc85db4cad5
R-Lashan <lashan779@gmail.com>
2021-08-24 07:59:40 +0530

    Added final changes to beaufify

--- BookApp/BookApp.SPA/src/components/Home.js
+++ BookApp/BookApp.SPA/src/components/Home.js
@@ -47,6 +47,7 @@ const Home = () => {
     body = 
     <div>
       <h1 className="text-2">Welcome to the BookApp</h1>
+      <p className="text-3">Here is the Gateway to all the Knowledge to Unlock your fullest Potential as an Individual. You can read Most Popular and Best Selling Books written by World Class Authors. Please Register to Gain the Knowledge and Explore the World of Books with BookApp.</p>
       <button className="btn signup-btn" onClick={() => history.push('/register')}>
         Register now
       </button>
@@ -58,6 +59,7 @@ const Home = () => {
       <div className="banner">
         {body}
       </div>
+      <span className="caption">Designed & Developed by Lashan @2021</span>
     </div>
   );
 }
